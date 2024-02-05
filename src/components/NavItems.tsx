dc874eac588672f9a8c544c6ddd947d9b612c3d7
Praveen <111781869+pscode22@users.noreply.github.com>
2024-02-05 23:03:07 +0530

    second commit

---
+++ src/components/NavItems.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+import { useState } from 'react';
+
+const NavItems = () => {
+  const [activeIndex, setActiveIndex] = useState<number | null>(null);
+  return <div className='flex gap-4 h-full'></div>;
+};
+export default NavItems;
